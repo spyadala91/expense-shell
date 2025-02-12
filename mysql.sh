6372609e5a483333ef9cefae4ab0874ada1a5b11
sai <spyadala91@gmail.com>
2025-02-12 22:28:50 +0000

    add conversation

--- mysql.sh
+++ mysql.sh
@@ -22,7 +22,7 @@ VALIDATE(){
 }
 
 CHECK_POINT(){
-    if [ $1 -ne 0 ]
+    if [ $USERID -ne 0 ]
     then
        echo "ERROR:: You must have sudo access to execute this script"
        exit 1 #other thank 0
