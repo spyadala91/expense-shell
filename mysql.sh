a65152c67da076889444a35b29772e83f41e35d3
sai <spyadala91@gmail.com>
2025-02-13 21:59:27 +0000

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -1,48 +1,50 @@
-    #!/bin/bash
+#!/bin/bash
 
-    USERID=$(id -u)
-    R="\e[31m"
-    G="\e[32m"
-    Y="\e[33m"
-    N="\e[0m"
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-    LOGS_FOLDER="/var/log/expense-logs"
-    LOG_FILE=$(echo $0 | cut -d "." -f1 )
-    TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-    LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(basename "$0" | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-    VALIDATE(){
-        if [ $1 -ne 0 ]
-        then
-           echo -e "$2 ... $R failure $N"
-           exit 1
-        else
+# Ensure log directory exists
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R failure $N"
+        exit 1
+    else
         echo -e "$2 ... $G success $N"
-        fi 
-    }
-
-    CHECK_POINT(){
-        if [ $USERID -ne 0 ]; then
-            echo "ERROR:: You must have sudo access to execute this script"
-            exit 1 #other than 0
-        fi
+    fi 
 }
 
-    echo "Script started executing at: $TIMESTAMP"
+CHECK_POINT(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R ERROR:: You must have sudo access to execute this script $N"
+        exit 1
+    fi
+}
 
-    CHECK_POINT
+echo "Script started executing at: $TIMESTAMP" | tee -a $LOG_FILE_NAME
 
-    dnf install mysql-server -y
-    VALIDATE $? "Installing MySQL Server"
+CHECK_POINT
 
-    systemctl daemon-reload
-    VALIDATE $? "reloading MySQL server"
-    
-    systemctl enable mysql
-    VALIDATE $? "Enabling MySQL Server"
+dnf install mysql-server -y &>> $LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Server"
 
-    systemctl start mysqld
-    VALIDATE $? "starting MySQL Server"
+systemctl daemon-reload &>> $LOG_FILE_NAME
+VALIDATE $? "Reloading MySQL Server"
 
-    mysql_secure_installation --set-root-pass ExpenseApp@1
-    VALIDATE $? "Setting Root Password"
+systemctl enable mysql &>> $LOG_FILE_NAME
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>> $LOG_FILE_NAME
+VALIDATE $? "Starting MySQL Server"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'ExpenseApp@1';" &>> $LOG_FILE_NAME
+VALIDATE $? "Setting Root Password"
