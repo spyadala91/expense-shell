640c8e52f908c803e8ea5fa9cb76c09962aca8bb
sai <spyadala91@gmail.com>
2025-02-13 20:50:33 +0000

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -25,7 +25,7 @@
         if [ $USERID -ne 0 ]
         then
             echo "ERROR:: You must have sudo access to execute this script"
-            exit 1 #other thank 0
+            exit 1 #other than 0
         fi
 }
 
