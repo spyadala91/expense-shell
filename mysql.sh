3a1601436291624903ae157b960e58d655f121ce
sai <spyadala91@gmail.com>
2025-02-13 20:23:52 +0000

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -1,53 +1,46 @@
-#!/bin/bash
+    #!/bin/bash
 
-USERID=$(id -u)
+    USERID=$(id -u)
     R="\e[31m"
     G="\e[32m"
     Y="\e[33m"
+    N="\e[0m"
+
     LOGS_FOLDER="var/log/expense-logs"
     LOG_FILE=$(echo $0 | cut -d "." -f1 )
     TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
     LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R failure $N"
-        exit 1
-    else
+    VALIDATE(){
+        if [ $1 -ne 0 ]
+        then
+           echo -e "$2 ... $R failure $N"
+           exit 1
+        else
         echo -e "$2 ... $G sucess $N"
-    fi 
-  
+        fi 
+    }
+
+    CHECK_POINT(){
+        if [ $USERID -ne 0 ]
+        then
+            echo "ERROR:: You must have sudo access to execute this script"
+            exit 1 #other thank 0
+        fi
 }
 
-CHECK_POINT(){
-    if [ $USERID -ne 0 ]
-    then
-       echo "ERROR:: You must have sudo access to execute this script"
-       exit 1 #other thank 0
-    fi
-}       
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+    echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-CHECK_POINT
+    CHECK_POINT
 
-dnf install mysql-server -y &>>$LOG_FILE_NAME
-VALIDATE $? "Installing mysql server"
+    dnf install mysql-server -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysql &>>$LOG_FILE_NAME
-VALIDATE $? "Enabling mysql server"
+    systemctl enable mysql &>>$LOG_FILE_NAME
+    VALIDATE $? "Enabling MySQL Server"
 
-systemctl start mysqld &>>$LOG_FILE_NAME
-VALIDATE $? "starting mysql server"
+    systemctl start mysqld &>>$LOG_FILE_NAME
+    VALIDATE $? "starting MySQL Server"
 
-mysql -h mysql.yadala.fun -u root -pExpenseApp@1 -e 'show databases;'
-
-if [ $? -ne 0 ]
-then 
-   echo "mysql Root password not setup" &>>$LOG_FILE_NAME
-   mysql_secure_installation --set-root-pass ExpenseApp@1
-   VALIDATE $? "setting root password"
-else
-   echo -e "mysql root password already setup ... $Y SKIPPING $N"
-fi   
+    mysql -h mysql.yadala.fun -u root -pExpenseApp@1 -e 'show databases;'
+    VALIDATE $? "Setting Root Password"
