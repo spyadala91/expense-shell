217b88f53a49a8dd3ada06f724242100dea9d23b
sai <spyadala91@gmail.com>
2025-02-13 21:19:02 +0000

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,6 @@
     LOGS_FOLDER="var/log/expense-logs"
     LOG_FILE=$(echo $0 | cut -d "." -f1 )
     TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-    LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
     VALIDATE(){
         if [ $1 -ne 0 ]
